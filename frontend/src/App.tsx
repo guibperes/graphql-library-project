3a557dc3cb769e114098e36f1725ddd82ede2d26
Guilherme Beidaki Peres <guibperes@protonmail.com>
2021-02-26 20:03:34 -0300

    added apollo client on App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 
-function App() {
+const errorLink = onError(({ graphQLErrors }) =>
+  graphQLErrors?.forEach(({ message }) => {
+    alert(`GraphQL error ${message}`);
+  })
+);
+
+const link = from([
+  errorLink,
+  new HttpLink({uri: 'http://localhost:4000/graphql'}),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
+export function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <ApolloProvider client={client}>Hello</ApolloProvider>
   );
 }
-
-export default App;
